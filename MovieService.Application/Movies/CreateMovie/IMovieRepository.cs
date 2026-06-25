57534b23df871cd556c200bcc17bb0542f42b11a
Nordic-Panda <odielee@hotmail.com>
2026-06-25 08:58:49 +0200

    feat: added movie repository contract for create-movie

---
+++ MovieService.Application/Movies/CreateMovie/IMovieRepository.cs
@@ -0,0 +1,10 @@
+﻿using MovieService.Domain.Entities;
+using MovieService.Domain.Movies;
+
+namespace MovieService.Application.Movies.CreateMovie
+{
+    public interface IMovieRepository
+    {
+        Task AddAsync(Movie movie);
+    }
+}
