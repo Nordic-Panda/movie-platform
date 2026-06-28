092687ebdc4a870b6c00f537eac5b93b630c4e75
Nordic-Panda <odielee@hotmail.com>
2026-06-28 21:51:59 +0200

    feat: put actor

---
+++ MovieService.Application/Actors/PutActor/PutActorValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MovieService.Domain.Actors;
+
+namespace MovieService.Application.Actors.PutActor
+{
+    public class PutActorValidator : AbstractValidator<PutActorCommand>
+    {
+        public PutActorValidator() 
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty();
+
+            RuleFor(x => x.FirstName)
+                .NotEmpty();
+
+            RuleFor(x => x.LastName)
+                .NotEmpty();
+
+            RuleFor(x => x.BirthYear)
+                .GreaterThanOrEqualTo(ActorRules.EarliestYear)
+                .LessThanOrEqualTo(DateTime.UtcNow.Year);
+        }
+    }
+}
