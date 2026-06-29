56009d9f3af8372e5271f003dcd33ef5cc823050
Nordic-Panda <odielee@hotmail.com>
2026-06-29 10:00:25 +0200

    feat: movieActor factory

--- MovieService.Application/Movies/AddActorToMovie/AddActorToMovieHandler.cs
+++ MovieService.Application/Movies/AddActorToMovie/AddActorToMovieHandler.cs
@@ -1,12 +1,22 @@
 ﻿using MediatR;
 using MovieService.Application.Common.DTOs;
+using MovieService.Application.Common.Interfaces.Repositories;
 namespace MovieService.Application.Movies.AddActorToMovie
 {
-    internal class AddActorToMovieHandler : IRequestHandler<AddActorToMovieCommand, MovieActorDto>
+    public class AddActorToMovieHandler : IRequestHandler<AddActorToMovieCommand, MovieActorDto>
     {
-        public Task<MovieActorDto> Handle(AddActorToMovieCommand request, CancellationToken cancellationToken)
+        private readonly IMovieActorRepository _movieActorRepository;
+
+        public AddActorToMovieHandler(IMovieActorRepository movieActorRepository)
+        {
+            _movieActorRepository = movieActorRepository;
+        }
+
+        public async Task<MovieActorDto> Handle(AddActorToMovieCommand command, CancellationToken cancellationToken)
         {
 
+
+            var movieActor = await _movieActorRepository.AddActorToMovieAsync();
             throw new NotImplementedException();
         }
     }
--- MovieService.Domain/MovieActor/MovieActor.cs
+++ MovieService.Domain/MovieActor/MovieActor.cs
@@ -15,25 +15,10 @@ namespace MovieService.Domain.Entities
 
         public MovieActor(Guid id, Guid movieId, Guid actorId, string characterName)
         {
-            if (movieId == Guid.Empty)
-                throw new DomainException(
-                    MovieActorErrors.MovieIdEmptyCode,
-                    MovieActorErrors.MovieIdEmptyMessage);
-
-            if (actorId == Guid.Empty)
-                throw new DomainException(
-                    MovieActorErrors.ActorIdEmptyCode,
-                    MovieActorErrors.ActorIdEmptyMessage);
-
-            if (string.IsNullOrWhiteSpace(characterName))
-                throw new DomainException(
-                    MovieActorErrors.CharacterNameEmptyCode,
-                    MovieActorErrors.CharacterNameEmptyMessage);
-
             Id = id;
             MovieId = movieId;
             ActorId = actorId;
-            CharacterName = characterName.Trim();
+            CharacterName = characterName;
         }
     }
 }
