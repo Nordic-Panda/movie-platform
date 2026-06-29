ce626c3a96b787aee39671152188fbbcf5057dac
Nordic-Panda <odielee@hotmail.com>
2026-06-29 13:37:12 +0200

    feat: added review factory

--- MovieService.Application/Reviews/CreateReview/CreateReviewHandler.cs
+++ MovieService.Application/Reviews/CreateReview/CreateReviewHandler.cs
@@ -1,13 +1,23 @@
 ﻿using MediatR;
 using MovieService.Application.Common.DTOs;
+using MovieService.Application.Common.Interfaces.Repositories;
 
 namespace MovieService.Application.Reviews.CreateReview
 {
     public class CreateReviewHandler : IRequestHandler<CreateReviewCommand, ReviewDto>
     {
+        private readonly IReviewRepository _reviewRepository;
 
-        public Task<ReviewDto> Handle(CreateReviewCommand request, CancellationToken cancellationToken)
+        public CreateReviewHandler(IReviewRepository reviewRepository)
         {
+            _reviewRepository = reviewRepository;
+        }
+
+        public async Task<ReviewDto> Handle(CreateReviewCommand request, CancellationToken cancellationToken)
+        {
+            //var review = ReviewFac
+            //await _reviewRepository.AddReviewAsync();
+            //await _reviewRepository.SaveChangesAsync();
             throw new NotImplementedException();
         }
     }
---
+++ MovieService.Domain/Reviews/ReviewFactory.cs
@@ -0,0 +1,21 @@
+﻿using MovieService.Domain.Exceptions;
+
+namespace MovieService.Domain.Reviews
+{
+    public static class ReviewFactory
+    {
+        public static Review Create(Guid movieId, string comment, int rating) 
+        {
+
+            if (rating < ReviewRules.MinRating ||
+                rating > ReviewRules.MaxRating)
+            {
+                throw new DomainException(
+                    ReviewErrors.InvalidRatingCode,
+                    ReviewErrors.InvalidRatingMessage(ReviewRules.MinRating, ReviewRules.MaxRating));
+            }
+
+            return new Review(Guid.NewGuid(), movieId, comment, rating);
+        }
+    }
+}
