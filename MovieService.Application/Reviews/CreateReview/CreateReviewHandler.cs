811c242f9b861e243457cf1626828ce053335b3c
Nordic-Panda <odielee@hotmail.com>
2026-06-29 13:22:29 +0200

    feat: repository

---
+++ MovieService.Application/Reviews/CreateReview/CreateReviewHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using MovieService.Application.Common.DTOs;
+
+namespace MovieService.Application.Reviews.CreateReview
+{
+    public class CreateReviewHandler : IRequestHandler<CreateReviewCommand, ReviewDto>
+    {
+
+        public Task<ReviewDto> Handle(CreateReviewCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MovieService.Infrastructure/Persistence/Reviews/ReviewRepository.cs
@@ -0,0 +1,26 @@
+﻿using MovieService.Application.Common.Interfaces.Repositories;
+using MovieService.Domain.Reviews;
+using MovieService.Infrastructure.Data;
+
+namespace MovieService.Infrastructure.Persistence.Reviews
+{
+    public class ReviewRepository : IReviewRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ReviewRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddReviewAsync(Review review)
+        {
+            //await _context.Reviews.AddAsync(review);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
