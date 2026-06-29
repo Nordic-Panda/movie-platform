ace14b513c6a10b095b004613973ea3385208719
Nordic-Panda <odielee@hotmail.com>
2026-06-29 09:40:18 +0200

    feat: controller, dto

---
+++ MovieService.Application/Common/DTOs/MovieActorDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieService.Application.Common.DTOs
+{
+    public class MovieActorDto
+    {
+        public Guid Id {  get; set; }
+        public Guid MovieId { get; set; }
+        public Guid ActorId { get; set; }
+        public string CharacterName { get; set; }
+    }
+}
---
+++ MovieService.Application/Common/Mappers/MovieActorMapper.cs
@@ -0,0 +1,11 @@
+﻿using MovieService.Application.Movies.AddActorToMovie;
+
+namespace MovieService.Application.Common.Mappers
+{
+    public static class MovieActorMapper
+    {
+        public static AddActorToMovieCommand ToAddActorToMovieCommand(Guid movieId, Guid actorId, string characterName) {
+            return new AddActorToMovieCommand(movieId, actorId, characterName);
+        }
+    }
+}
