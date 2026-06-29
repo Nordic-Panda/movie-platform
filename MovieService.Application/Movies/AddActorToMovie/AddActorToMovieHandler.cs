b33d07ac5b26eb527ebd3ea706d829cc08ac4f04
Nordic-Panda <odielee@hotmail.com>
2026-06-29 10:10:51 +0200

    fix: wrong name for MovieErrors

--- MovieService.Application/Movies/AddActorToMovie/AddActorToMovieHandler.cs
+++ MovieService.Application/Movies/AddActorToMovie/AddActorToMovieHandler.cs
@@ -1,24 +1,36 @@
 ﻿using MediatR;
 using MovieService.Application.Common.DTOs;
+using MovieService.Application.Common.Exceptions;
 using MovieService.Application.Common.Interfaces.Repositories;
 using MovieService.Application.Common.Mappers;
 using MovieService.Domain.MovieActor;
+using MovieService.Domain.Movies;
 namespace MovieService.Application.Movies.AddActorToMovie
 {
     public class AddActorToMovieHandler : IRequestHandler<AddActorToMovieCommand, MovieActorDto>
     {
         private readonly IMovieActorRepository _movieActorRepository;
+        private readonly IMovieRepository _movieRepository;
 
-        public AddActorToMovieHandler(IMovieActorRepository movieActorRepository)
+        public AddActorToMovieHandler(IMovieActorRepository movieActorRepository, IMovieRepository movieRepository)
         {
             _movieActorRepository = movieActorRepository;
+            _movieRepository = movieRepository;
         }
 
         public async Task<MovieActorDto> Handle(AddActorToMovieCommand command, CancellationToken cancellationToken)
         {
+            var movie = await _movieRepository.GetByIdAsync(command.MovieId);
+
+            if (movie == null)
+                throw new NotFoundException(MovieErrors.MovieNotFoundCode, MovieErrors.MovieNotFoundMessage);
+
+
+
             var movieActor = MovieActorFactory.Create(command.MovieId, command.ActorId, command.CharacterName);
 
             await _movieActorRepository.AddActorToMovieAsync(movieActor);
+            await _movieActorRepository.SaveChangesAsync();
 
             return MovieActorMapper.ToDto(movieActor);
         }
--- MovieService.Domain/Movies/MovieErrors.cs
+++ MovieService.Domain/Movies/MovieErrors.cs
@@ -1,6 +1,6 @@
 ﻿namespace MovieService.Domain.Movies
 {
-    public class ActorErrors
+    public class MovieErrors
     {
         public const string TitleEmptyCode = "MOVIE_EMPTY_TITLE";
         public const string TitleEmptyMessage = "Movie title is required";
