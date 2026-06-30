099facc8e814ed62f9412c1b67bba9ea3776432b
Nordic-Panda <odielee@hotmail.com>
2026-06-30 09:29:35 +0200

    refactor: extract EF Core entity configurations from DbContext

---
+++ MovieService.Infrastructure/Persistence/MovieActors/MovieActorConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MovieService.Domain.Entities;
+using MovieService.Domain.Movies;
+namespace MovieService.Infrastructure.Persistence.MovieActors
+{
+    internal class MovieActorConfiguration : IEntityTypeConfiguration<MovieActor>
+    {
+        void IEntityTypeConfiguration<MovieActor>.Configure(EntityTypeBuilder<MovieActor> entity)
+        {
+            entity.HasKey(x => x.Id);
+
+            entity.Property(x => x.MovieId)
+                .IsRequired();
+
+            entity.Property(x => x.ActorId)
+                .IsRequired();
+
+            entity.Property(x => x.CharacterName)
+                .IsRequired();
+
+            entity.HasIndex(x => new { x.MovieId, x.ActorId, x.CharacterName })
+                .IsUnique();
+
+            // Cascade delete, on delete all Entity that has relation to this will be deleted
+
+            entity.HasOne<Movie>()
+                .WithMany()
+                .HasForeignKey(x => x.Id)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
