2f4e414f7f0f7c91853a38a9286cd28e4cab89ad
Nordic-Panda <odielee@hotmail.com>
2026-07-01 20:02:05 +0200

    feat: added user repository

---
+++ MovieService.API/Contollers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieService.API.Contollers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        //[HttpPost("login")]
+        //public async Task<IActionResult> Login([FromBody] LoginCommand request) 
+        //{
+            
+        //}
+    }
+}
---
+++ MovieService.Infrastructure/Persistence/Users/UsersRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieService.Application.Common.Interfaces.Repositories;
+using MovieService.Domain.Users;
+using MovieService.Infrastructure.Data;
+
+namespace MovieService.Infrastructure.Persistence.Users
+{
+    public class UsersRepository : IUserRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public UsersRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<User?> GetUserByEmailAsync(string email)
+        {
+            return await _appDbContext.Users.FirstOrDefaultAsync(x => x.Email == email);
+        }
+    }
+}
