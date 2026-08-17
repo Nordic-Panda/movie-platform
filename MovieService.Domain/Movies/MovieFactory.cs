0cd6cfea3423708ab8376e73fcbe489c44f23939
Nordic-Panda <odielee@hotmail.com>
2026-08-17 14:26:01 +0200

    fix: wrong param

--- MovieService.Domain/Movies/MovieFactory.cs
+++ MovieService.Domain/Movies/MovieFactory.cs
@@ -10,39 +10,40 @@ namespace MovieService.Domain.Movies
             string title,
             TimeSpan duration,
             IReadOnlyCollection<Genre> genres,
-            MovieDetails details)
+            MovieDetails details
+        )
         {
             if (string.IsNullOrWhiteSpace(title))
                 throw new DomainException(
                     MovieErrors.TitleEmptyCode,
-                    MovieErrors.TitleEmptyMessage);
+                    MovieErrors.TitleEmptyMessage
+                );
 
             if (title.Length < MovieRules.TitleMinLength)
                 throw new DomainException(
                     MovieErrors.TitleTooShortCode,
-                    MovieErrors.TitleTooShortMessage(MovieRules.TitleMinLength));
+                    MovieErrors.TitleTooShortMessage(MovieRules.TitleMinLength)
+                );
 
             if (title.Length > MovieRules.TitleMaxLength)
                 throw new DomainException(
                     MovieErrors.TitleTooLongCode,
-                    MovieErrors.TitleTooLongMessage(MovieRules.TitleMaxLength));
+                    MovieErrors.TitleTooLongMessage(MovieRules.TitleMaxLength)
+                );
 
             if (duration < MovieRules.MinDuration)
                 throw new DomainException(
                     MovieErrors.DurationTooShortCode,
-                    MovieErrors.DurationTooShortMessage((int)MovieRules.MinDuration.TotalMinutes));
+                    MovieErrors.DurationTooShortMessage((int)MovieRules.MinDuration.TotalMinutes)
+                );
 
             if (duration > MovieRules.MaxDuration)
                 throw new DomainException(
                     MovieErrors.DurationTooLongCode,
-                    MovieErrors.DurationTooLongMessage((int)MovieRules.MinDuration.TotalMinutes));
+                    MovieErrors.DurationTooLongMessage((int)MovieRules.MaxDuration.TotalMinutes)
+                );
 
-            return new Movie(
-                Guid.NewGuid(),
-                title,
-                duration,
-                genres,
-                details);
+            return new Movie(Guid.NewGuid(), title, duration, genres, details);
         }
     }
 }
