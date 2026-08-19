1cb38ac32fd763442d640017abb7428e1f7789e3
Nordic-Panda <odielee@hotmail.com>
2026-08-19 15:18:54 +0200

    refactor: max length is no longer hard coded

--- MovieService.Application/Genres/CreateGenre/CreateGenreValidator.cs
+++ MovieService.Application/Genres/CreateGenre/CreateGenreValidator.cs
@@ -1,13 +1,13 @@
 ﻿using FluentValidation;
+using MovieService.Domain.Genres;
+
 namespace MovieService.Application.Genres.CreateGenre
 {
     public class CreateGenreValidator : AbstractValidator<CreateGenreCommand>
     {
         public CreateGenreValidator()
         {
-            RuleFor(x => x.Name)
-                .NotEmpty()
-                .MaximumLength(100);
+            RuleFor(x => x.Name).NotEmpty().MaximumLength(GenreRules.TitleMaxLength);
         }
     }
 }
