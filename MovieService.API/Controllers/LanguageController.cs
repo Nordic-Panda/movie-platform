0e8d610936d36bbd5f2163f20679f7544ff45c0f
Nordic-Panda <odielee@hotmail.com>
2026-08-19 12:50:23 +0200

    feat: added language controller, get languages pipeline done

---
+++ MovieService.API/Controllers/LanguageController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MovieService.API.Common.Contracts;
+using MovieService.Application.Common.DTOs;
+using MovieService.Application.Languages.GetLanguages;
+
+namespace MovieService.API.Controllers
+{
+    [Route("api/languages")]
+    [ApiController]
+    public class LanguageController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LanguageController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetLanguages()
+        {
+            var languages = await _mediator.Send(new GetLanguagesQuery());
+            return Ok(ApiResponse<IReadOnlyList<LanguageDto>>.Ok(languages));
+        }
+    }
+}
