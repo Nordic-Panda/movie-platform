00164e3d15f30ebb949ba968b8312327e40f116d
Nordic-Panda <odielee@hotmail.com>
2026-08-20 21:02:34 +0200

    refactor: money factory

--- MovieService.Domain/Moneies/MoneyFactory.cs
+++ MovieService.Domain/Moneies/MoneyFactory.cs
@@ -1,5 +1,4 @@
-﻿using MovieService.Domain.Common.Exceptions;
-using MovieService.Domain.Currencies;
+﻿using MovieService.Domain.Currencies;
 using MovieService.Domain.ValueObjects;
 
 namespace MovieService.Domain.Moneies
@@ -8,17 +7,7 @@ namespace MovieService.Domain.Moneies
     {
         public static Money Create(decimal amount, Currency currency)
         {
-            if (amount < 0)
-                throw new DomainException(
-                    MoneyErrors.NegativeAmountCode,
-                    MoneyErrors.NegativeAmountMessage
-                );
-
-            if (currency is null)
-                throw new DomainException(
-                    MoneyErrors.MissingCurrencyCode,
-                    MoneyErrors.MissingCurrencyMessage
-                );
+            MoneyRules.ValidateBudgetAmount(amount);
 
             return new Money(amount, currency);
         }
---
+++ MovieService.Domain/Moneies/MoneyRules.cs
@@ -0,0 +1,18 @@
+﻿using MovieService.Domain.Common.Exceptions;
+
+namespace MovieService.Domain.Moneies
+{
+    public class MoneyRules
+    {
+        public const int BudgetMinValue = 0;
+
+        public static void ValidateBudgetAmount(decimal amount)
+        {
+            if (amount < BudgetMinValue)
+                throw new DomainException(
+                    MoneyErrors.NegativeAmountCode,
+                    MoneyErrors.NegativeAmountMessage
+                );
+        }
+    }
+}
